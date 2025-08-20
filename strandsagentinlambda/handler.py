df4e5f6df2706badec664e9241a5398b5738298a
Muhammed Basil <basil@montycloud.com>
2025-08-20 12:11:59 +0000

    Simple strands agent in a lambda

---
+++ strandsagentinlambda/handler.py
@@ -0,0 +1,14 @@
+from strands import Agent
+import json
+
+
+
+agent = Agent()
+
+def call_agent(event, context):
+# Ask the agent a question
+    resp = agent("Tell me about agentic AI")
+    return {
+        'statusCode': 200,
+        'body': json.dumps(resp)
+    }
